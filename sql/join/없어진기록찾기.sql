26f84b4dfb4ff3465ab59130bbfe867c89327a94
spagettiUser <tqbs1024@gmail.com>
2020-06-01 22:14:49 +0900

    feat: 🎸 없어진기록찾기, 있었는데없어졌습니다., 오랜기간보호한동물 sql

---
+++ sql/join/없어진기록찾기.sql
@@ -0,0 +1,1 @@
+select o.ANIMAL_ID, o.name from ANIMAL_INS i right join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID where i.ANIMAL_ID is null
---
+++ sql/join/있었는데요없어졌습니다.sql
@@ -0,0 +1,1 @@
+select i.animal_id, i.name from animal_ins i left join animal_outs o on i.animal_id = o.animal_id where i.datetime>o.datetime order by i.datetime asc
