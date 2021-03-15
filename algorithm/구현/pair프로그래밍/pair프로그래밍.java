eab1f8b3b685da32b9603882346f6bdde879818a
lllilllilllilili <hik3562@hufs.ac.kr>
2021-03-16 00:48:17 +0900

    feat: 🎸 Add pair프로그래밍

---
+++ algorithm/구현/pair프로그래밍/pair프로그래밍.java
@@ -0,0 +1,56 @@
+
+public class p1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1번
+//		int[][] input = { 
+//				{1, 2}, 
+//				{2, 1}, 
+//				{3, 3}, 
+//				{4, 5}, 
+//				{5, 6}, 
+//				{7, 8}
+//		};
+		
+		//2번
+//		int[][] input = {
+//				{1, 2}, 
+//				{3, 4}, 
+//				{5, 6}
+//		};
+		
+		//3번
+		int[][] input = {
+				{1, 2}, 
+				{2, 3}, 
+				{3, 1}
+		};
+		
+		int answer = 0;
+		int box_cnt = input.length;
+		int[] product = new int[1000001];
+		//step1 : product 전수 조사 
+		for(int i=0; i<input.length; i++) {
+			product[input[i][0]]+=1;
+			product[input[i][1]]+=1;
+		}//eofl
+		
+		//setp2 : 2미만 개수에 대해 정답을 +1 단, 상자의 개수를 초과하면 더이상 조회하지 않음
+		for(int i=0; i<product.length; i++) {
+			if(box_cnt == 0) break;
+			if(product[i]==0) continue;
+			else if(product[i]%2==0) {
+				box_cnt--;
+			}else {
+				box_cnt--;
+				answer+=1;
+			}
+		}//eofl
+		StringBuilder sb = new StringBuilder();
+		sb.append(answer);
+		System.out.println(sb.toString());
+		
+	}
+
+}
